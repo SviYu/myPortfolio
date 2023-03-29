5dc93598a5dd71d7afdcb5370d617275841579be
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2023-03-29 14:24:21 +0200

    Updating projects

--- js/projects.js
+++ js/projects.js
@@ -5,14 +5,14 @@ window.addEventListener('load', () => {
 })
 
 /* Progress animation */
-window.onscroll = function() { myFunction() };
+/* window.onscroll = function() { myFunction() };
 
 function myFunction() {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrolled = (winScroll / height) * 100;
     document.getElementById('myBar').style.width = scrolled + '%'
-}
+} */
 
 /* Current Year */
 const yearSpan = document.getElementById('year');
